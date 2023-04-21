9d8360afd2de4926f612e5b35248c154f26cb4ef
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-21 23:59:20 +0900

    DB 스키마 정의하는 내용과, _id를 todoId 가상컬럼으로 보여줄 코드 생성

---
+++ models/todo.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+    value:String,   // 할일이 어떤 것인지 확인하는 컬럼
+    doneAt:Date,    // 할일이 언제 완료되었는지 확인하는 컬럼
+    order:Number    // 몇번째 할일인지 확인하는 컬럼
+});
+
+// Frontend를 사용하기 위해 필요한 것 : 조회를 했을 때 가상으로 만들어지는 컬럼
+// mongoDB에 있는 _id 컬럼을 todoId로 클라이언트에게 보여줄 것임!!
+// .toHexString() : _id 의 혹시라도 생길 에러를 줄여주기 위한 문자열 변환 메서드
+TodoSchema.virtual("todoId")
+.get(()=>this._id.toHexString());
+
+// TodoSchema를 외부로 내보내주기
+module.exports = mongoose.model("Todo", TodoSchema);
