66f37af64561f3f20a133bdafe31fc7afb5ff242
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-21 22:07:00 +0900

    app.js add: comment

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require("express");
+const express = require("express");   // express library import
+const app = express();                // express app
+const router = express.Router();      // express Router
+const PORT = '8080';                  // HTTP Port :8080
 
-const app = express();
-const router = express.Router();
-
-const PORT = '8080';
-
+// HTTP Root "/" - Front-end
 router.get("/", (req, res) => {
   res.send("Hi!");
 });
+
+// HTTP Router Root "/api" - Back-end
 app.use("/api", express.json(), router);
 
-
-
+// HTTP Port listen
 app.listen(PORT, () => {
   console.log(`${PORT}번 포트로 서버가 켜졌어요!`);
 });
