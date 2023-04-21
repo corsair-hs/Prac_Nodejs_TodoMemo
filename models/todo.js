06903ae6c51570d4692a6e4485e8b69e826f2f6b
AllA-5900HX\AllA <hsk.computer.1@gmail.com>
2023-04-22 00:03:44 +0900

    todo.js : TodoSchema의 todoId를 JSON으로 변환하였을 때 어떻게 보여줄 지 정의하는 코드 추가

--- models/todo.js
+++ models/todo.js
@@ -9,8 +9,11 @@ const TodoSchema = new mongoose.Schema({
 // Frontend를 사용하기 위해 필요한 것 : 조회를 했을 때 가상으로 만들어지는 컬럼
 // mongoDB에 있는 _id 컬럼을 todoId로 클라이언트에게 보여줄 것임!!
 // .toHexString() : _id 의 혹시라도 생길 에러를 줄여주기 위한 문자열 변환 메서드
-TodoSchema.virtual("todoId")
-.get(()=>this._id.toHexString());
+TodoSchema.virtual("todoId").get(()=>this._id.toHexString());
+
+// TodoSchema를 사용하기 위해서 어떤 타입으로 변경을 했을 때 보여줄 거냐에 대한 설정
+// 위의 todoId를 JSON 형태로 변환하였을 때 보여준다라는 의미
+TodoSchema.set("toJSON", {virtuals: ture});
 
 // TodoSchema를 외부로 내보내주기
 module.exports = mongoose.model("Todo", TodoSchema);
